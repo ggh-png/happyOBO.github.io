cd653229774ab48bcb2b6af9b9921598a0b01b77
happyOBO <obo314@icloud.com>
2020-07-22 17:11:28 +0900

    solve acmicpc

--- assets/code_box/bk_2798.py
+++ assets/code_box/bk_2798.py
@@ -1,1 +1,12 @@
-a , b = map(int, input().split)
+N, M = map(int, input().split())
+card_list = list(map(int, input().split()))
+max = 0
+for i in range(N):
+    for j in range(N):
+        if( i != j):
+            for k in range(N):
+                if( i != k and j != k):
+                    m = card_list[i] + card_list[j] + card_list[k]
+                    if( max < m and m <= M):
+                        max = m
+print(max)
