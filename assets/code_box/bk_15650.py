ea7849b4fde142ffa370a2059e42be852b0a05ed
happyOBO <obo314@icloud.com>
2020-07-22 20:04:44 +0900

    solve acmicpc

---
+++ assets/code_box/bk_15650.py
@@ -0,0 +1,24 @@
+## 백트래킹
+# 입력
+N, M = map(int, input().split())
+
+orders = []
+for i in range(M):
+    orders.append(0)
+# 백트랙킹 함수
+def back(idx,order_list):
+    ord_list = order_list[:]
+    if(idx >= M):
+        # 완성된 수열 출력
+        for i in range(M-1):
+            print(ord_list[i],end = " ")
+        print(ord_list[M-1])
+    else:
+        st = 1 if (idx == 0) else order_list[idx-1] + 1
+        for i in range(st ,N+1):
+            # 이전 idx 위치의 배열 값보다 큰 값 할당
+            if(not i in order_list):
+                ord_list[idx] = i
+                back(idx + 1, ord_list)
+
+back(0,orders)
