9587780cb3ac7db98c855fab37b6f306d091c579
happyOBO <obo314@icloud.com>
2020-07-23 15:46:26 +0900

    add ocaml_print

---
+++ assets/code_box/bk_9663.py
@@ -0,0 +1,19 @@
+# 퀸은 앞, 뒤, 대각선으로 움직임
+# 백트래킹
+N = int(input(()))
+
+# 이중 배열 초기화
+arr = [[0]*N for _ in range(N)]
+count = 0
+
+def check_straight ():
+
+def check_diagonal ():
+
+def back():
+    if(end):
+        count += 현재 배열에서 0의 개수 추가
+    else :
+        
+
+
