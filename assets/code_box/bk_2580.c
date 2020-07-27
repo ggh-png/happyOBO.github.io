190143a92d4f6c8cca88d5cc586471b9ef683b0a
happyOBO <obo314@icloud.com>
2020-07-27 17:00:08 +0900

    add failed_sudoku

--- assets/code_box/bk_2580.c
+++ assets/code_box/bk_2580.c
@@ -4,7 +4,8 @@
 int blank_count = 0 ;
 int sudoku[15][15] = { 0, };
 int blank_idx[100][5] = { 0, };
-
+// 반례 참고 https://cryptosalamander.tistory.com/59
+// 444444hello3hello2hello2hellohellohellohellohellohello43324
 // int is_in(int num, int arr[] ,int arr_leng)
 // {
 //     for(int i = 0; i< arr_leng; i++)
@@ -16,24 +17,24 @@ int blank_idx[100][5] = { 0, };
 
 void back(int n,int fill_blanks[])
 {
-    // int k =0;
-    // for(int i = 0; i<9; i++)
-    // {
-    //     for(int j = 0; j<9; j++)
-    //     {
-    //         if(sudoku[i][j] == 0)
-    //         {
-    //             printf("%d",fill_blanks[k++]);
-    //         }
-    //         else
-    //         {
-    //             printf("%d",sudoku[i][j]);
-    //         }
-    //         if(j < 8) printf(" ");
-    //     }
-    //     printf("\n");
-    // }
-    // printf("\n");
+    int k =0;
+    for(int i = 0; i<9; i++)
+    {
+        for(int j = 0; j<9; j++)
+        {
+            if(sudoku[i][j] == 0 && j < n)
+            {
+                printf("%d",fill_blanks[k++]);
+            }
+            else
+            {
+                printf("%d",sudoku[i][j]);
+            }
+            if(j < 8) printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
     if(n == blank_count)
     {
         int k = 0;
@@ -64,10 +65,12 @@ void back(int n,int fill_blanks[])
             int smallcube_col = blank_idx[n][1] / 3;
             for(int j = 0; j < 9; j++)
             {
-                if(n == 10) printf("%d ",sudoku[blank_idx[n][0]][j]);
-                if(sudoku[j][blank_idx[n][1]] == i || sudoku[j][blank_idx[n][0]] == i )
+                // if(i == 7) printf("hello");
+                if(sudoku[j][blank_idx[n][1]] == i || sudoku[blank_idx[n][0]][j] == i )
                 {
+                    // printf("hello!\n");
                     is_fill = 0;
+                    // printf("4");
                     break;
                 }
                 if( j < 3)
@@ -75,10 +78,10 @@ void back(int n,int fill_blanks[])
                     for(int p = smallcube_col* 3 ; p < smallcube_col* 3 + 3; p++)
                     {
                         
-                        if( n ==3 && blank_idx[n][0] == 2 && blank_idx[n][1] == 0)
-                        {
-                            printf("sudoku [%d] [%d] = %d",smallcube_row * 3 + j,p,sudoku[smallcube_row * 3 + j][p]);
-                        }
+                        // if( n ==3 && blank_idx[n][0] == 2 && blank_idx[n][1] == 0)
+                        // {
+                        //     printf("sudoku [%d] [%d] = %d",smallcube_row * 3 + j,p,sudoku[smallcube_row * 3 + j][p]);
+                        // }
                         if(sudoku[smallcube_row * 3 + j][p] == 0)
                         {
                             for(int u = 0; u < n;u++)
@@ -88,19 +91,24 @@ void back(int n,int fill_blanks[])
                                     if(fill_blanks[u] == i)
                                     {
                                         is_fill = 0;
+                                        // printf("1");
                                         break;
                                     }
                                 }
                             }
                             if(!is_fill)
                             {
+                                // printf("2");
                                 break;
                             }
                         }
                         else if(sudoku[smallcube_row * 3 + j][p] == i )
                         {
 
+                            // printf("sudoku[%d][%d] = %d",smallcube_row * 3 + j,p,i);
+
                             is_fill = 0;
+                            // printf("3");
                             break;                        
                         }
 
@@ -112,7 +120,7 @@ void back(int n,int fill_blanks[])
             {
                 fill_blanks[n] = i;
                 back(n+1,fill_blanks);
-                // fill_blanks[n] = 0;
+                fill_blanks[n] = 0;
             }
         }
     }
