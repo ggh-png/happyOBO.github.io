fa3c8c09b458cdf34688700e74befced52d9127c
happyOBO <obo314@icloud.com>
2020-09-28 13:25:00 +0900

    add l_T

---
+++ assets/code_box/pg_42840.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    
+    return answer;
+}
